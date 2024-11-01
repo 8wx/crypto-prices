9dbe2ee503e880341a201b1748d13ec9228876a1
Archer <84819659+8wx@users.noreply.github.com>
2024-11-02 01:02:19 +0200

    Update updatePrices.js

--- netlify/functions/updatePrices.js
+++ netlify/functions/updatePrices.js
@@ -1,5 +1,8 @@
 // netlify/functions/updatePrices.js
 exports.handler = async function(event, context) {
+    console.log('Event:', event); // Log the entire event object
+    console.log('Context:', context); // Log the entire context object
+    
     // Log the event type
     console.log('Event type:', event.type);
     console.log('Is scheduled?:', !!event.isScheduled);
@@ -15,6 +18,7 @@ exports.handler = async function(event, context) {
             })
         };
     } catch (error) {
+        console.error('Error:', error); // Log the error
         return {
             statusCode: 500,
             body: JSON.stringify({ error: error.message })
